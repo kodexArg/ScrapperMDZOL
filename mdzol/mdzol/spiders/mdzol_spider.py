602b81aa56cfa418f0e0a80a108204cc7d3514fb
KodexArg <gcavedal@gmail.com>
2022-04-10 23:09:45 -0300

    MDZ nows follow link to get epigraph and body (edit: body fixed)

--- mdzol/mdzol/spiders/mdzol_spider.py
+++ mdzol/mdzol/spiders/mdzol_spider.py
@@ -1,8 +1,11 @@
 import scrapy
 
+
 # xpath valid for mdzol.com:
 NEWS_LINK = "//h2[@class='news__title']/a/@href"
 NEWS_TEXT = "//h2[@class='news__title']/a/text()"
+NEWS_EPIGRAPH = "//p[starts-with(@class,'epigraph')]/text()"
+NEWS_BODY = "//div[@class='modules-container']//p[not(@class)]//text()"
 BAN_LINKS = ['/estilo', 'https:/www.quever']
 
 
@@ -15,7 +18,6 @@ class MdzolSpider(scrapy.Spider):
                        'FEED_EXPORT_ENCODING': 'utf-8'}
 
     open(custom_settings['FEED_URI'], 'w').close()
-
 
     def parse(self, response):
         nnews = {}
@@ -30,19 +32,33 @@ class MdzolSpider(scrapy.Spider):
             for ban_startswith in BAN_LINKS:
                 if nlink[k].startswith(ban_startswith):
                     discard = True
-
-            if discard: continue
+            if discard:
+                continue
 
             for filter in filters:
                 if text.lower().find(filter) != -1:
-                    nnews[k] = {
+                    nnews = {
                         "text": text,
                         "link": nlink[k]
                     }
-    
-        yield nnews
-    
+
+                    yield response.follow(nlink[k], callback=self.parse_link, cb_kwargs=nnews)
+
 
     def parse_link(self, response, **kwargs):
-        pass
+        nnews = kwargs
 
+        epigraph = response.xpath(NEWS_EPIGRAPH).get()
+        epigraph = epigraph.replace('\n', '')
+        nnews['epigraph'] = epigraph
+
+        # it's not possible to get the paragraph with <p></p>, so the body's div needs parsing:
+        body = response.xpath(NEWS_BODY).getall()
+        body = ''.join(body) #
+        for k, _ in enumerate(body):
+            if len(body) > k + 3:
+                if body[k] == "." and body[k+1] in "ABCDEFGHIJKLMNOPQRSTUVWXYZÁÉÍÓÚ":
+                    body = body[:k] + ".\n" + body[k + 1:]
+        nnews['body'] = body.replace(" "," ").replace("\n\n","\n")
+
+        yield nnews
